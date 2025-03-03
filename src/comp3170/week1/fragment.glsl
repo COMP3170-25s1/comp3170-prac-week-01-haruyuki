b62aaecda1c031b57b6fa3c8cc108729022e465d
Haru <jumpy12359@gmail.com>
2025-03-03 12:27:23 +1100

    Update files

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -1,4 +1,4 @@
-#version 410
+ #version 410
 
 uniform vec3 u_colour;			// colour as a 3D vector (r,g,b)
 uniform vec2 u_screenSize;  	// screen dimensions in pixels
@@ -7,12 +7,21 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   
+   float aspectRatio = u_screenSize.x / u_screenSize.y;
+   
+   vec2 centre = p - vec2(0.5, 0.5);
+   // float d = distance(p, vec2(0.2, 0.2));     // calculate distance to midpoint
+   
+   vec2 v = abs(centre);
+   float d = v.x + v.y;
+   
   
-   if (d < 0.5) {
+   if (d < 0.2) {
       o_colour = vec4(u_colour, 1);
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(0.102, 0.310, 0.729, 1); // BLACK
    }
+  
 }
